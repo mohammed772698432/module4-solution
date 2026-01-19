0eaa56e5112cec6bd4c9a8ce17061863b70f4d8e
mohammed salah mohammed hasan <160389227+mohammed772698432@users.noreply.github.com>
2026-01-19 22:13:16 +0300

    Module 4 Coding Assignment Solution
    This project demonstrates basic JavaScript concepts such as arrays, loops,
    conditionals, and IIFE by printing Hello or Good Bye messages to the console.

---
+++ assignment4-solution-starter/easier/SpeakHello.js
@@ -0,0 +1,17 @@
+(function (window) {
+
+  // STEP 3
+  var helloSpeaker = {};
+
+  // DO NOT attach the speakWord variable to the 'helloSpeaker' object.
+  var speakWord = "Hello";
+
+  // STEP 4
+  helloSpeaker.speak = function (name) {
+    console.log(speakWord + " " + name);
+  };
+
+  // STEP 5
+  window.helloSpeaker = helloSpeaker;
+
+})(window);
